a9cd1a9550761ef1af3bc5f41382b4d553549920
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-15 16:22:56 -0500

    not done but pushing what I have

--- src/main/java/Vehicles/HondaCivic.java
+++ src/main/java/Vehicles/HondaCivic.java
@@ -3,6 +3,15 @@ package Vehicles;
 import Driving.Car;
 
 public class HondaCivic extends Car {
+    private boolean needsOilChange;
+    private final double topSpeed;
+    private Double totalDistance;
+
+    public HondaCivic() {
+        needsOilChange = false;
+        topSpeed = 70.0;
+        totalDistance = 0.0;
+    }
     /**
      * A civic should need an oil change every
      * 50,000 miles traveled. Once the oil is
@@ -13,7 +22,15 @@ public class HondaCivic extends Car {
      */
     @Override
     public Boolean needsOilChange() {
-        return null;
+        //every 50000 miles
+        //if you buy a new car, you don't need to change the oil, start changing the oil after 50000
+        if(getDistanceTraveled() / 50000 > 0) {
+            this.needsOilChange = true;
+        }
+        else {
+            this.needsOilChange = false;
+        }
+        return this.needsOilChange;
     }
 
     /**
@@ -25,7 +42,7 @@ public class HondaCivic extends Car {
      */
     @Override
     public void changeOil() {
-
+        this.needsOilChange = false;
     }
 
     /**
@@ -37,7 +54,7 @@ public class HondaCivic extends Car {
      */
     @Override
     public Boolean checkEngineLight() {
-        return null;
+        return needsOilChange();
     }
 
     /**
@@ -50,7 +67,7 @@ public class HondaCivic extends Car {
      */
     @Override
     public Double getDistanceTraveled() {
-        return null;
+        return this.totalDistance;
     }
 
     /**
@@ -59,12 +76,12 @@ public class HondaCivic extends Car {
      */
     @Override
     public Double getTopSpeed() {
-        return null;
+        return this.topSpeed;
     }
 
     /**
      * transport should calculate the time it takes in
-     * seconds to travel a distance base on the top
+     * seconds to travel a distance based on the top
      * speed and update the amount of miles traveled.
      *
      * @param distance - length of travel in miles
@@ -72,6 +89,14 @@ public class HondaCivic extends Car {
      */
     @Override
     public Integer transport(Double distance) {
-        return null;
+        //first part converts the time it takes for a trip into seconds
+        //trip time in hours, it could be like 3500 miles / 28.5 (the top speed of the bike) which would be 122 hours (after truncating)
+        Integer timeInHours = (int) (distance / getTopSpeed());
+        Integer timeInSeconds = timeInHours * 3600;
+
+        //update amount of miles traveled
+        this.totalDistance += distance;
+
+        return timeInSeconds;
     }
 }
--- src/main/java/Vehicles/MountainBike.java
+++ src/main/java/Vehicles/MountainBike.java
@@ -11,12 +11,12 @@ public class MountainBike extends Bike {
     public MountainBike() {
         this.RECOMMENDEDTIREPRESSURE = 30;
         this.TOPSPEED = 28.5;
-        this.actualTirePressure = 0;
-        this.actualSpeed = 0.0;
+        this.actualTirePressure = 30;
+        this.actualSpeed = 28.5;
     }
     /**
      * The top speed of a mountain bike should be 28.5
-     * but for every 1 PSI under the recommended tire
+     * but for every 1 PSI of the current tire pressure under the recommended tire
      * pressure the tires of the bike are, the top
      * speed should be reduced 1mph.
      *
@@ -24,8 +24,12 @@ public class MountainBike extends Bike {
      */
     public Double getTopSpeed() {
         Double actualSpeed = this.TOPSPEED;
+        //if the tires are full of air, then there will be no speed reduction
+        //thus actual speed will stay the same, but if the actualTirePressure is less than the recommended (meaning you don't have a tire full of air, potentially a flat tire), then you won't be going so fast
+        //so the top speed will decrease as a result
         int speedReduction = 0;
         if(this.actualTirePressure < this.RECOMMENDEDTIREPRESSURE) {
+            //say you have a tire pressure of 28 and the recommended is 30. Then you would need to subtract 2 PIS
            speedReduction = this.RECOMMENDEDTIREPRESSURE - this.actualTirePressure;
         }
         actualSpeed -= speedReduction;
@@ -35,7 +39,7 @@ public class MountainBike extends Bike {
 
     /**
      * transport should calculate the time it takes in
-     * seconds to travel a distance base on the top
+     * seconds to travel a distance based on the top
      * speed and update the tire pressure. Long trips
      * on a mountain bike will reduce its PSI, so for
      * every 30 miles traveled in a single trip, the
@@ -47,19 +51,26 @@ public class MountainBike extends Bike {
      * @return time in seconds to travel distance
      */
     public Integer transport(Double distance) {
-        Double actualSpeed = this.TOPSPEED;
         //first part converts the time it takes for a trip into seconds
-        Integer timeInHours = (int) (distance / getTopSpeed());
-        Integer timeInMinutes = timeInHours / 60;
-        Integer timeInSeconds = timeInHours / 3600;
+        Integer timeInHours = 0;
+        //trip time in hours, it could be like 3500 miles / 28.5 (the top speed of the bike) which would be 122 hours (after truncating)
+        timeInHours = (int) (distance / getTopSpeed());
+        Integer timeInSeconds = timeInHours * 3600;
 
+        //if you do Math.round then you'll get the wrong answer because for 29.9 it rounds it up to 30, and 30/30 is 1, not 0, which it should be if its 29.9/30
+        Integer distanceInt = distance.intValue();
         //gives amount of reductions for every 30 minutes
-        Integer amountToReduce = timeInMinutes % 30;
+        //will return 0 if less than 30
+        Integer amountToReduce = distanceInt / 30;
         Integer newTirePressure = getTirePressure() - amountToReduce;
-        if(newTirePressure >= 20) {
+        //if the new tire pressure is less than 20, set the tire pressure to 20
+        if(newTirePressure < 20) {
+            this.actualTirePressure = 20;
+        }
+        //the new tire pressure is greater than 20, so set the actualTirePressure to tirePressure
+        else {
             this.actualTirePressure = newTirePressure;
         }
-
         return timeInSeconds;
     }
 
@@ -79,7 +90,7 @@ public class MountainBike extends Bike {
      */
     @Override
     public void inflateTires() {
-
+        this.actualTirePressure = this.RECOMMENDEDTIREPRESSURE;
     }
 
     /**
